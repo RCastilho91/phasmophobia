82aab839e1ead131cc9e4752000381142dff189c
Rodrigo M. Castilho <rodrigo.m.castilho@outlook.com>
2022-07-02 21:25:51 -0300

    Add initial icons

---
+++ src/icons/orbs.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+
+
+const Orbs = ({ color }) => (
+<svg
+    xmlnsRdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsCc="http://creativecommons.org/ns#"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlnsDc="http://purl.org/dc/elements/1.1/"
+    xmlnsSvg="http://www.w3.org/2000/svg"
+    xmlnsInkscape="http://www.inkscape.org/namespaces/inkscape"
+    xmlnsSodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+    xmlnsNs1="http://sozi.baierouge.fr"
+    id="svg2"
+    viewBox="0 0 48 48"
+    version="1.1"
+    fill={color}
+  >
+  <g
+      id="layer1"
+      transform="translate(-2248 35.586)"
+    >
+    <g
+        id="g5335"
+        transform="matrix(2.0285 0 0 2.0285 -2935.2 668.82)"
+      >
+      <path
+          id="path5337"
+          transform="matrix(-1 0 0 -1 2879 -292.61)"
+          d="m315 35.815c0 1.6569-1.3432 3-3 3s-3-1.3431-3-3 1.3432-3 3-3 3 1.3431 3 3z"
+      />
+      <path
+          id="path5339"
+          transform="matrix(-1 0 0 -1 2883 -294.61)"
+          d="m315 35.815c0 1.6569-1.3432 3-3 3s-3-1.3431-3-3 1.3432-3 3-3 3 1.3431 3 3z"
+      />
+      <path
+          id="path5341"
+          transform="matrix(-1 0 0 -1 2884 -298.61)"
+          d="m315 35.815c0 1.6569-1.3432 3-3 3s-3-1.3431-3-3 1.3432-3 3-3 3 1.3431 3 3z"
+      />
+      <path
+          id="path5343"
+          transform="matrix(-1 0 0 -1 2880 -301.61)"
+          d="m315 35.815c0 1.6569-1.3432 3-3 3s-3-1.3431-3-3 1.3432-3 3-3 3 1.3431 3 3z"
+      />
+      <path
+          id="path5345"
+          transform="matrix(-1 0 0 -1 2875 -300.61)"
+          d="m315 35.815c0 1.6569-1.3432 3-3 3s-3-1.3431-3-3 1.3432-3 3-3 3 1.3431 3 3z"
+      />
+      <path
+          id="path5347"
+          transform="matrix(-1 0 0 -1 2873 -296.61)"
+          d="m315 35.815c0 1.6569-1.3432 3-3 3s-3-1.3431-3-3 1.3432-3 3-3 3 1.3431 3 3z"
+      />
+      <path
+          id="path5349"
+          transform="matrix(-1 0 0 -1 2875 -293.61)"
+          d="m315 35.815c0 1.6569-1.3432 3-3 3s-3-1.3431-3-3 1.3432-3 3-3 3 1.3431 3 3z"
+      />
+      <path
+          id="path5351"
+          transform="matrix(-1.3333 0 0 -1.3333 2982 -284.67)"
+          d="m315 35.815c0 1.6569-1.3432 3-3 3s-3-1.3431-3-3 1.3432-3 3-3 3 1.3431 3 3z"
+      />
+      <path
+          id="path5353"
+          transform="translate(2248 -499.24)"
+          d="m324 156.82c0 0.55228-0.44772 1-1 1s-1-0.44772-1-1 0.44772-1 1-1 1 0.44772 1 1z"
+      />
+      <path
+          id="path5355"
+          transform="translate(2242 -502.24)"
+          d="m324 156.82c0 0.55228-0.44772 1-1 1s-1-0.44772-1-1 0.44772-1 1-1 1 0.44772 1 1z"
+      />
+      <path
+          id="path5357"
+          transform="translate(2236 -483.24)"
+          d="m324 156.82c0 0.55228-0.44772 1-1 1s-1-0.44772-1-1 0.44772-1 1-1 1 0.44772 1 1z"
+      />
+      <path
+          id="path5359"
+          transform="translate(2252 -482.24)"
+          d="m324 156.82c0 0.55228-0.44772 1-1 1s-1-0.44772-1-1 0.44772-1 1-1 1 0.44772 1 1z"
+      />
+      <path
+          id="path5361"
+          transform="translate(2237 -499.24)"
+          d="m324 156.82c0 0.55228-0.44772 1-1 1s-1-0.44772-1-1 0.44772-1 1-1 1 0.44772 1 1z"
+      />
+    </g
+    >
+  </g
+  >
+</svg
+>
+
+);
+
+
+export default Orbs;
